2a3cbbb294dc3ca540a0ddf1df511c68ccf06165
Cauã fróes <bkcauafroes2012@gmail.com>
2023-06-12 17:04:24 -0300

    sendreports

--- src/components/SendReport.tsx
+++ src/components/SendReport.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEvent } from "react";
 import Switch from "react-switch";
+import Slider from "@mui/material/Slider";
 import api from "./api/api";
 
 interface FormData {
@@ -14,6 +15,9 @@ interface FormData {
 export default function SendReport() {
   const [automaticGPS, setAutomaticGPS] = useState(false);
   const [value, setValue] = useState(0); // Set do Input Range
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [red, setRed] = useState(255);
+  const [green, setGreen] = useState(200);
   const [data, setData] = useState<FormData>({
     name: "",
     desc: "",
@@ -28,11 +32,11 @@ export default function SendReport() {
   };
   const getRangeColor = () => {
     if (value < 3) {
-      return 'bg-red-500';
+      return "bg-red-500";
     } else if (value < 6) {
-      return 'bg-yellow-500';
+      return "bg-yellow-500";
     } else {
-      return 'bg-green-500';
+      return "bg-green-500";
     }
   };
 
@@ -72,8 +76,8 @@ export default function SendReport() {
     formData.append("cep", data.cep);
     formData.append("gps_lat", String(data.gps_lat));
     formData.append("gps_long", String(data.gps_long));
-    if (data.image) {
-      formData.append("image", data.image);
+    if (selectedFile) {
+      formData.append("image", selectedFile);
     }
     try {
       const response = await api.post("/reports", formData, {
@@ -82,8 +86,15 @@ export default function SendReport() {
         },
       });
       console.log(response.data);
-    } catch (error) {
+    } catch (error: any) {
       alert(JSON.stringify(error.response.data.message));
+    }
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]);
     }
   };
 
@@ -115,18 +126,33 @@ export default function SendReport() {
       }));
     }
   };
+
+  const handleSliderChange = (event: any, value: any) => {
+    const normalizedValue = value / 10;
+    const updatedRed = Math.round(normalizedValue + 1 * 200);
+    const updatedGreen = Math.round((1 - normalizedValue) * 200);
+
+    setRed(updatedRed);
+    setGreen(updatedGreen);
+  };
+
+  const colorStyle = {
+    width: 300,
+    color: `rgb(${red}, ${green}, 60)`,
+  };
+
   return (
     <>
-      <div className="max-w-md p-7 mx-auto">
+      <div className="max-w-md p-7 mb-28 mx-auto">
         <h1 className="text-2xl font-bold mb-4">Reportar lixo</h1>
         <form onSubmit={handleSubmit}>
           <label className="block mb-2">
-            Data:
+            Nome:
             <input
-              type="date"
               name="name"
               value={data.name}
               onChange={handleChange}
+              placeholder="Lixo na esquina da minha casa!"
               className="border border-gray-300 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </label>
@@ -136,7 +162,18 @@ export default function SendReport() {
               name="desc"
               value={data.desc}
               onChange={handleChange}
+              placeholder="Estão jogando lixo..."
               className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </label>
+          <label className="flex items-center mt-5 mb-5 justify-between bg-blue-400 p-4 rounded-xl text-white">
+            Usar minha localização
+            <Switch
+              height={17}
+              width={35}
+              handleDiameter={25}
+              onChange={handleSwitchChange}
+              checked={automaticGPS}
             />
           </label>
           {automaticGPS ? (
@@ -148,7 +185,7 @@ export default function SendReport() {
                   name="gps_lat"
                   value={data.gps_lat || ""}
                   onChange={handleChange}
-                  className="border border-gray-300 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="border border-gray-300 text-gray-400 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </label>
               <label className="block mb-2">
@@ -158,7 +195,7 @@ export default function SendReport() {
                   name="gps_long"
                   value={data.gps_long || ""}
                   onChange={handleChange}
-                  className="border border-gray-300 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="border border-gray-300 text-gray-400 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </label>
             </>
@@ -168,28 +205,19 @@ export default function SendReport() {
               <input
                 name="cep"
                 value={data.cep || ""}
+                placeholder="12345-678"
                 onChange={handleChange}
                 className="border border-gray-300 rounded px-2 py-1 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </label>
           )}
-          <label className="flex items-center justify-between mb-1 text-slate-400">
-            Usar localização automática
-            <Switch
-              height={17}
-              width={35}
-              handleDiameter={25}
-              onChange={handleSwitchChange}
-              checked={automaticGPS}
-            />
-          </label>
           <label className="block mb-1 pt-1">
             Selecione uma imagem:
             <input
               type="file"
               accept="image/*"
               name="image"
-              onChange={handleChange}
+              onChange={handleFileChange}
               className="
                 file:bg-gradient-to-b file:from-blue-400 file:to-blue-500
                 file:text-xs
@@ -202,20 +230,33 @@ export default function SendReport() {
               "
             />
           </label>
-          <label htmlFor="rangeInput" className="flex mb-1 justify-between items-center">
+          <p>Quantidade de Lixo</p>
+          <label
+            htmlFor="rangeInput"
+            className="flex mb-1 justify-between items-center"
+          >
             <span>0</span>
             <span>10</span>
           </label>
-            <input
-            id="rangeInput" 
+          <input
+            id="rangeInput"
             type="range"
-            min="0" 
+            min="0"
             max="10"
             value={value}
             onChange={handleInputChange}
             className={`w-full h-8 rounded-md transition-colors ${getRangeColor()}`}
-            />
-          
+          />
+          <Slider
+            defaultValue={2}
+            min={0}
+            max={10}
+            sx={colorStyle}
+            onChange={handleSliderChange}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+          />
           <button
             type="submit"
             className="bg-blue-500 mt-3 text-white py-4 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
--- src/components/api/api.tsx
+++ src/components/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.15.38/api",
+  baseURL: "http://localhost/api",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
