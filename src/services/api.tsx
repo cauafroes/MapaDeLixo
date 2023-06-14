0618e853060ddb7b92583a9c29f4ab48b56ffd23
fróes <67842825+cauafroes@users.noreply.github.com>
2023-06-14 18:00:26 -0300

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.0.1.137/api",
+  baseURL: "https://froesmhs.com/mapalixoapi/public/api/",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
